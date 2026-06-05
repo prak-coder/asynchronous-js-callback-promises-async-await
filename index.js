fe5b6d1c6379d71f0101ebe3434416f51f6c18cf
prak-coder <prakash.change@gmail.com>
2026-06-05 11:53:03 +0530

    IIFE with asyn/await to get returned data from async func

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ async function getDogPicture() {
     console.log(res.body.message);
     await fs.writeFile(`${__dirname}/dogImagePath.txt`, res.body.message);
     console.log("image path written in file");
+    return "2.second log";
   } catch (error) {
     console.log(error);
   }
 }
-getDogPicture();
+
+//iife
+(async () => {
+  try {
+    console.log("1.getting dog pic");
+    const x = await getDogPicture();
+    console.log(x);
+    console.log("3.done getting doc pics");
+  } catch (error) {
+    console.log(error);
+  }
+})();
